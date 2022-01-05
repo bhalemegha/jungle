470a76e544d10874c21eae63486495d324519459
bhalemegha <your@email.com>
2022-01-05 19:40:32 +0000

    Fix empty cart issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,10 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart
 
-  def enhanced_cart
+  def enhanced_cart    
     @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
   end
+  
   helper_method :enhanced_cart
 
   def cart_subtotal_cents
